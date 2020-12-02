d5d7898d3b3da01217a141f3bd4c9f4ddb6cb645
Nguyễn Thái An <thaian229@gmail.com>
2020-12-03 00:17:33 +0700

    Coding CreditCardManager and PaymentTransactionManager

--- Programming/EBR/src/model/payment/creditCard/CreditCardManager.java
+++ Programming/EBR/src/model/payment/creditCard/CreditCardManager.java
@@ -1,6 +1,10 @@
 package model.payment.creditCard;
 
 
+import model.db.EBRDB;
+
+import java.sql.*;
+
 public class CreditCardManager {
 
     private static CreditCardManager instance;
@@ -12,8 +16,66 @@ public class CreditCardManager {
         return instance;
     }
 
+    public CreditCard getCardById(String cardId) {
+        // query the card
+        String SQL = "SELECT * FROM card " +
+                "WHERE id = ?";
 
-    public CreditCard getCardById(String card_id) {
+        try (Connection conn = EBRDB.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+            // Set up parameters
+            pstmt.setString(1, cardId);
+            // Handle result set
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+
+            return new CreditCard(
+                    rs.getString("id"),
+                    rs.getString("card_num"),
+                    rs.getString("card_owner"),
+                    Integer.parseInt(rs.getString("security_code")),
+                    rs.getString("exp_date")
+            );
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
+
+    public String saveCreditCard(CreditCard creditCard) {
+        String SQL = "INSERT INTO card(card_num, card_owner, security_code, exp_date) " +
+                "VALUES (?, ?, ?, ?)";
+
+        String id = "save failed";
+
+        // Insert new row
+        try (Connection conn = EBRDB.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(SQL,
+                     Statement.RETURN_GENERATED_KEYS)) {
+            // Set up parameters
+            pstmt.setString(1, creditCard.getCardNum());
+            pstmt.setString(2, creditCard.getCardOwner());
+            pstmt.setString(3, Integer.toString(creditCard.getSecurityCode()));
+            pstmt.setString(4, creditCard.getExpDate());
+            // Handle update
+            int affectedRows = pstmt.executeUpdate();
+            // check the affected rows
+            if (affectedRows > 0) {
+                // get the id back
+                try (ResultSet rs = pstmt.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        id = rs.getString("id");
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return id;
+    }
+
 }
--- Programming/EBR/src/model/payment/transaction/PaymentTransaction.java
+++ Programming/EBR/src/model/payment/transaction/PaymentTransaction.java
@@ -3,12 +3,17 @@ package model.payment.transaction;
 import model.payment.creditCard.CreditCard;
 
 public class PaymentTransaction {
+
 	private String errorCode;
 	private CreditCard card;
 	private String transactionId;
 	private String transactionContent;
 	private int amount;
 	private String createdAt;
+
+	private String id;
+	private String type;
+	private String method;
 	
 	public PaymentTransaction(String errorCode, CreditCard card, String transactionId, String transactionContent,
 			int amount, String createdAt) {
@@ -20,8 +25,31 @@ public class PaymentTransaction {
 		this.amount = amount;
 		this.createdAt = createdAt;
 	}
+
+	public PaymentTransaction(String id, String type, int amount, String method) {
+		this.id = id;
+		this.type = type;
+		this.amount = amount;
+		this.method = method;
+	}
 	
 	public String getErrorCode() {
 		return errorCode;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getMethod() {
+		return method;
 	}
 }
