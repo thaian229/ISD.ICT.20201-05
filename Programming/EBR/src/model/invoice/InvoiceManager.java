9052e06adf4250497a641ce95b7c05f232748d8b
nguyenbim <51375333+nguyenbim@users.noreply.github.com>
2020-12-02 23:05:49 +0700

    modify InvoiceManager

--- Programming/EBR/src/model/invoice/InvoiceManager.java
+++ Programming/EBR/src/model/invoice/InvoiceManager.java
@@ -1,36 +1,44 @@
 package model.invoice;
 
+
+import model.bike.Bike;
+import model.payment.CreditCard;
+import model.payment.PaymentTransaction;
 import model.session.Session;
 
 import java.util.ArrayList;
 
-public class InvoiceHistory {
-    private static InvoiceHistory instance;
+public class InvoiceManager {
+    private static InvoiceManager instance;
     private ArrayList<Invoice> invoiceHistory;
-    public InvoiceHistory(){
+    public InvoiceManager(){
         this.invoiceHistory = new ArrayList<>();
     }
 
-    public InvoiceHistory getInstance(){
+    public InvoiceManager getInstance(){
         if(instance == null){
-            instance = new InvoiceHistory();
+            instance = new InvoiceManager();
         }
         return instance;
     }
     public ArrayList<Invoice> getInvoiceHistory(){
         return invoiceHistory;
     }
-
+    public Invoice createInvoice(Session session, Bike bike, CreditCard card, PaymentTransaction returnPaymentTransaction){
+        Invoice newInvoice = new Invoice(session,bike,card,returnPaymentTransaction);
+        this.addInvoice(newInvoice);
+        return newInvoice;
+    }
     public void setInvoiceHistory(ArrayList<Invoice> invoiceHistory) {
         this.invoiceHistory = invoiceHistory;
     }
-    public void addInvoice(Invoice invoice){
-        invoiceHistory.add(invoice);
+    public void addInvoice(Invoice newInvoice){
+        invoiceHistory.add(newInvoice);
     }
     public void removeInvoice(Invoice invoice){
         invoiceHistory.remove(invoice);
     }
-    public Invoice searchInvoiceById(String invoiceId){
+    public Invoice getInvoiceById(String invoiceId){
         for(Invoice invoice: invoiceHistory){
             if(invoice.getId() == invoiceId) return invoice;
         }
