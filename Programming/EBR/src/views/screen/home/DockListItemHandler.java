c6b219ad9396b09c37aa5dd4d3e307d4a83d320b
mHoang99 <vmhoang1999@gmail.com>
2020-12-03 23:13:34 +0700

    UPDATE 4

---
+++ Programming/EBR/src/views/screen/home/DockListItemHandler.java
@@ -0,0 +1,21 @@
+package views.screen.home;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import views.screen.BaseScreenHandler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DockListItemHandler extends BaseScreenHandler implements Initializable {
+
+    public DockListItemHandler(Stage stage, String screenPath) throws IOException {
+        super(stage, screenPath);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
