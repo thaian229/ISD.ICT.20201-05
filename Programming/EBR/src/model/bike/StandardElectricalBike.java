bc587e780604147ecfd895da0fa78921254cf5ad
Bùi Tú Hoàng <64259366+buituhoang@users.noreply.github.com>
2020-12-05 22:47:43 +0700

    Generate javadoc

--- Programming/EBR/src/model/bike/StandardElectricalBike.java
+++ Programming/EBR/src/model/bike/StandardElectricalBike.java
@@ -1,20 +1,28 @@
 package model.bike;
 
+/**
+ * Model for standard electrical bike
+ *
+ * @author Bui Tu Hoang
+ * <p>
+ * creted at: 24/11/2020
+ * <p>
+ * project name: EBR
+ * <p>
+ * teacher's name: Dr. Nguyen Thi Thu Trang
+ * <p>
+ * class name: TT.CNTT ICT 02 - K62
+ */
 public class StandardElectricalBike extends Bike {
     private float battery;
     private String timeLeft;
 
-    public StandardElectricalBike(int barcode){
-        this.barcode = barcode;
-        this.saddle = 1;
-        this.rearSeat = 1;
-        this.pairOfPedals = 1;
-        this.deposit = 4000000;
-        this.charge = 50000;
-        this.battery = (float) 100;
-        this. timeLeft = "6 hours";
-    }
-
+    /**
+     * Constructor
+     * @param barcode Standard electrical bike's barcode
+     * @param value Standard electrical bike's value
+     * @param charge Standard electrical bike's renting fee
+     */
     public StandardElectricalBike(int barcode, int value, int charge){
         this.barcode = barcode;
         this.saddle = 1;
@@ -27,6 +35,13 @@ public class StandardElectricalBike extends Bike {
         this. timeLeft = "6 hours";
     }
 
+    /**
+     * Most use constructor
+     * @param id Standard electrical bike's uuid
+     * @param barcode Standard electrical bike's barcode
+     * @param value Standard electrical bike's value
+     * @param charge Standard electrical bike's renting fee
+     */
     public StandardElectricalBike(String id, int barcode, int value, int charge){
         this.id = id;
         this.barcode = barcode;
--- Programming/EBR/src/model/bike/TwinBike.java
+++ Programming/EBR/src/model/bike/TwinBike.java
@@ -1,5 +1,18 @@
 package model.bike;
 
+/**
+ * Model for twin bike
+ *
+ * @author Bui Tu Hoang
+ * <p>
+ * creted at: 24/11/2020
+ * <p>
+ * project name: EBR
+ * <p>
+ * teacher's name: Dr. Nguyen Thi Thu Trang
+ * <p>
+ * class name: TT.CNTT ICT 02 - K62
+ */
 public class TwinBike extends Bike{
     public TwinBike(int barcode) {
         this.barcode = barcode;
@@ -10,6 +23,12 @@ public class TwinBike extends Bike{
         this.charge = 20000;
     }
 
+    /**
+     * Constructor
+     * @param barcode Twin bike's barcode
+     * @param value Twin bike's value
+     * @param charge Twin bike's renting fee
+     */
     public TwinBike(int barcode, int value, int charge) {
         this.barcode = barcode;
         this.saddle = 2;
@@ -20,6 +39,13 @@ public class TwinBike extends Bike{
         this.charge = charge;
     }
 
+    /**
+     * Most use constructor
+     * @param id Twin bike's uuid
+     * @param barcode Twin bike's barcode
+     * @param value Twin bike's value
+     * @param charge Twin bike's renting fee
+     */
     public TwinBike(String id, int barcode, int value, int charge) {
         this.id = id;
         this.barcode = barcode;
