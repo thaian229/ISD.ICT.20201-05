999a943679b9d944257d790e4965f51cc5a30faf
Nguyễn Thái An <thaian229@gmail.com>
2020-12-14 15:43:03 +0700

    Fix Interbank and API

--- Programming/EBR/src/subsystem/interbank/InterbankBoundary.java
+++ Programming/EBR/src/subsystem/interbank/InterbankBoundary.java
@@ -2,14 +2,13 @@ package subsystem.interbank;
 
 import common.exception.UnrecognizedException;
 import utils.API;
-import utils.Utils;
 
 public class InterbankBoundary {
 
 	String query(String url, String data) {
 		String response = null;
 		try {
-			response = API.post(url, Utils.md5(data));
+			response = API.post(url, data);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			throw new UnrecognizedException();
--- Programming/EBR/src/subsystem/interbank/InterbankSubsystemController.java
+++ Programming/EBR/src/subsystem/interbank/InterbankSubsystemController.java
@@ -15,27 +15,35 @@ public class InterbankSubsystemController {
 	private static final String SECRET_KEY = "BBbFAE8bdPY=";
 	private static final String PAY_COMMAND = "pay";
 	private static final String REFUND_COMMAND = "refund";
-	private static final String VERSION = "1.0.0";
+	private static final String VERSION = "1.0.1";
 
 	private static final InterbankBoundary interbankBoundary = new InterbankBoundary();
 
 	public PaymentTransaction refund(CreditCard card, int amount, String contents) {
+		// set up transaction
 		Map<String, Object> transaction = new MyMap();
-
+		transaction.put("command", REFUND_COMMAND);
 		transaction.put("cardCode", card.getCardNum());
 		transaction.put("owner", card.getCardOwner());
 		transaction.put("cvvCode", card.getSecurityCode());
 		transaction.put("dateExpired", card.getExpDate());
-
-		transaction.put("command", REFUND_COMMAND);
 		transaction.put("transactionContent", contents);
 		transaction.put("amount", amount);
 		transaction.put("createdAt", Utils.getToday());
 
+		// generate hash code
+		String hashCode;
+		Map<String, Object> toBeHash = new MyMap();
+		toBeHash.put("secretKey", SECRET_KEY);
+		toBeHash.put("transaction", transaction);
+		hashCode = Utils.md5(generateData(toBeHash));
+
+		// set up body
 		Map<String, Object> requestMap = new MyMap();
 		requestMap.put("version", VERSION);
-		requestMap.put("secretKey", SECRET_KEY);
 		requestMap.put("transaction", transaction);
+		requestMap.put("appCode", PUBLIC_KEY);
+		requestMap.put("hashCode", hashCode);
 
 		String responseText = interbankBoundary.query(Configs.PROCESS_TRANSACTION_URL, generateData(requestMap));
 		MyMap response = null;
@@ -47,34 +55,33 @@ public class InterbankSubsystemController {
 		}
 
 		return makePaymentTransaction(response);
-	}
-	
-	private String generateData(Map<String, Object> data) {
-		return ((MyMap) data).toJSON();
 	}
 
 	public PaymentTransaction payDeposit(CreditCard card, int amount, String contents) {
+		// set up transaction
 		Map<String, Object> transaction = new MyMap();
-
+		transaction.put("command", PAY_COMMAND);
 		transaction.put("cardCode", card.getCardNum());
 		transaction.put("owner", card.getCardOwner());
 		transaction.put("cvvCode", card.getSecurityCode());
 		transaction.put("dateExpired", card.getExpDate());
-//		try {
-//			transaction.putAll(MyMap.toMyMap(card));
-//		} catch (IllegalArgumentException | IllegalAccessException e) {
-//			// TODO Auto-generated catch block
-//			throw new InvalidCardException();
-//		}
-		transaction.put("command", PAY_COMMAND);
 		transaction.put("transactionContent", contents);
 		transaction.put("amount", amount);
 		transaction.put("createdAt", Utils.getToday());
 
+		// generate hash code
+		String hashCode;
+		Map<String, Object> toBeHash = new MyMap();
+		toBeHash.put("secretKey", SECRET_KEY);
+		toBeHash.put("transaction", transaction);
+		hashCode = Utils.md5(generateData(toBeHash));
+
+		// set up body
 		Map<String, Object> requestMap = new MyMap();
 		requestMap.put("version", VERSION);
-		requestMap.put("secretKey", SECRET_KEY);
 		requestMap.put("transaction", transaction);
+		requestMap.put("appCode", PUBLIC_KEY);
+		requestMap.put("hashCode", hashCode);
 
 		String responseText = interbankBoundary.query(Configs.PROCESS_TRANSACTION_URL, generateData(requestMap));
 		MyMap response = null;
@@ -88,34 +95,8 @@ public class InterbankSubsystemController {
 		return makePaymentTransaction(response);
 	}
 
-	public PaymentTransaction returnDeposit(CreditCard card, int amount, String contents) {
-		Map<String, Object> transaction = new MyMap();
-
-		try {
-			transaction.putAll(MyMap.toMyMap(card));
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			throw new InvalidCardException();
-		}
-		transaction.put("command", PAY_COMMAND);
-		transaction.put("transactionContent", contents);
-		transaction.put("amount", amount);
-		transaction.put("createdAt", Utils.getToday());
-
-		Map<String, Object> requestMap = new MyMap();
-		requestMap.put("version", VERSION);
-		requestMap.put("transaction", transaction);
-
-		String responseText = interbankBoundary.query(Configs.PROCESS_TRANSACTION_URL, generateData(requestMap));
-		MyMap response = null;
-		try {
-			response = MyMap.toMyMap(responseText, 0);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			throw new UnrecognizedException();
-		}
-
-		return makePaymentTransaction(response);
+	private String generateData(Map<String, Object> data) {
+		return ((MyMap) data).toJSON();
 	}
 
 	private PaymentTransaction makePaymentTransaction(MyMap response) {
